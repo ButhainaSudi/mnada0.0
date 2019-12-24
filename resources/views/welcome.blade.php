bfbdf4623b45c0c6faf3186758a93ca9ad982961
Buthaina <buthainasudi@gmail.com>
2019-12-24 20:21:58 +0800

    Title for the project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MNADA LEO
                 </div>
 
                 <div class="links">
